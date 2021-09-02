c20049e8524cbc69dcd015fbe848f836f3026bac
Dusan <duqian2010@gmail.com>
2021-09-02 19:03:14 +0800

    修复编译问题

--- example/src/main/java/com/dicedmelon/example/android/MainActivity.java
+++ example/src/main/java/com/dicedmelon/example/android/MainActivity.java
@@ -1,11 +1,11 @@
 package com.dicedmelon.example.android;
 
+import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import com.dicedmelon.example.android.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
